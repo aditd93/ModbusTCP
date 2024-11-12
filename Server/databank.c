3105e95f9bf9305f99154ba52a7a76190198a3da
aditd93 <aditd1993@gmail.com>
2024-11-12 22:31:50 +0200

    Update files names

---
+++ Server/databank.c
@@ -0,0 +1,25 @@
+#include "datbank.h"
+
+DataBank *create_DB(int coils_num,int discrete_inputs_num,int inputs_regs_num,int holding_regs_num) {
+    DataBank *DB = (DataBank*)malloc(sizeof(DataBank));
+    if(DB == NULL) {
+        fprintf(stderr, "Failed to allocate memory for new DataBank\n");
+        exit(EXIT_FAILURE);
+    }
+    DB->coils_num = coils_num;    DB->discrete_num = discrete_inputs_num;    
+    DB->input_regs_num = inputs_regs_num;    DB->holding_regs_num = holding_regs_num;
+    DB->coils = (coils_num > 0) ? (bool*)calloc(coils_num,sizeof(bool)) : NULL;
+    DB->discrete_regs = (discrete_inputs_num > 0) ? (bool*)calloc(discrete_inputs_num,sizeof(bool)) : NULL;
+    DB->input_regs = (inputs_regs_num > 0) ? (uint16_t*)calloc(inputs_regs_num,sizeof(uint16_t)) : NULL;
+    DB->holding_regs = (holding_regs_num > 0) ? (uint16_t*)calloc(holding_regs_num,sizeof(uint16_t)) : NULL;
+    if((DB->coils == NULL && coils_num > 0) || (DB->discrete_regs == NULL && discrete_inputs_num > 0) || (DB->input_regs == NULL && inputs_regs_num > 0) || (DB->holding_regs == NULL && holding_regs_num > 0)) {
+        fprintf(stderr, "ERROR: Failed to allocate memory for new DataBank\n");
+        free(DB);
+        exit(EXIT_FAILURE);
+    }
+    return DB;
+}
+
+void Modify_DB(DataBank *DB, MBAP *mbap) {
+    // DataBank modification due to write request coming from a client
+}
---
+++ Server/datbank.h
@@ -0,0 +1,44 @@
+#ifndef DATABANK_H_
+#define DATABANK_H_
+#include "../MBAP.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/types.h>
+
+// Variables and Macros
+#define READ_COILS 0x01
+#define READ_DISCRETE_INPUTS 0x02
+#define READ_MULTIPLE_HOLDING_REGISTERS 0x03
+#define READ_INPUT_REGISTERS 0X04
+#define WRITE_SINGLE_COIL 0x05
+#define WRITE_SINGLE_HOLDING_REGISTER 0X06
+
+// Server's databank of registers
+
+typedef struct DataBank {
+    int coils_num, discrete_num, input_regs_num, holding_regs_num;
+    bool *coils;            // Read/Write discrete inputs
+    bool *discrete_regs;    // Read only discrete inputs
+    uint16_t *input_regs;   // Read only 16-bits registers
+    uint16_t *holding_regs; // Read/Write 16-bits registers
+} DataBank;
+
+/*  
+    Notes:
+    |                         - MBAP -               |       - PDU -        |
+    =========================================================================
+    | Transaction ID| Protocol ID | Length | Unit ID | Fucntion code | Data |
+    =========================================================================
+    |                                - ADU -                                |
+*/
+
+// functions
+DataBank *create_DB(int coils_num,int discrete_inputs_num,int inputs_regs_num,int holding_regs_num);
+void Modify_DB(DataBank *DB);
+
+
+#endif /* DATABANK_HEADER_H_ */
